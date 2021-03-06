ba283494658c44870d30933afc08e77ad90fe88e
daiki <morilr.tabletennis@gmail.com>
2021-03-06 23:04:36 +0900

    Braveクラスに必殺技メソッドを追加

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ class Brave
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
+  SPECIAL_ATTACK_CONSTANT = 1.5
 
   def initialize(params)
     @name = params[:name]
@@ -11,12 +12,26 @@ class Brave
   end
 
   def attack(target)
-    damege = self.offense - target.defense
+    attack_num = rand(4)
+
+    if attack_num == 0
+      p "#{self.name}の必殺技"
+      damege = calculate_special_attack - target.defense
+    else
+      p "#{self.name}の攻撃"
+      damege = self.offense - target.defense
+    end
+
     target.hp -= damege
+
     p <<~TEXT
       #{self.name}は#{target.name}に#{damege}のダメージを与えた
       #{target.name}の残りHPは#{target.hp}だ
     TEXT
+  end
+
+  def calculate_special_attack
+    self.offense * SPECIAL_ATTACK_CONSTANT
   end
 
 end
