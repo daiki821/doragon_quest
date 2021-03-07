7a4a9b126631834281602cd98527c354f1d5bc71
daiki <morilr.tabletennis@gmail.com>
2021-03-07 15:24:10 +0900

    モンスタークラスにも攻撃用のメソッドを追加

--- main.rb
+++ main.rb
@@ -11,22 +11,22 @@ class Brave
     @defense = params[:defense]
   end
 
-  def attack(target)
+  def attack(monster)
     attack_num = rand(4)
 
     if attack_num == 0
       p "#{self.name}の必殺技"
-      damege = calculate_special_attack - target.defense
+      damege = calculate_special_attack - monster.defense
     else
       p "#{self.name}の攻撃"
-      damege = self.offense - target.defense
+      damege = self.offense - monster.defense
     end
 
-    target.hp -= damege
+    monster.hp -= damege
 
     p <<~TEXT
-      #{self.name}は#{target.name}に#{damege}のダメージを与えた
-      #{target.name}の残りHPは#{target.hp}だ
+      #{self.name}は#{monster.name}に#{damege}のダメージを与えた
+      #{monster.name}の残りHPは#{monster.hp}だ
     TEXT
   end
 
@@ -37,21 +37,61 @@ class Brave
 end
 
 class Monster
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  attr_accessor :name, :hp, :transformed, :offense, :defense
 
+  POWER_UP_RATE = 1.5
+  CALC_HP_HALF = 0.5
+  
   def initialize(params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+    @transformed =  false
+    @hp_half = params[:hp] * CALC_HP_HALF
   end
+
+  def attack(brave)
+    if self.hp <= @hp_half && self.transformed == false
+      transformation
+      damege = self.offense - brave.defense
+    else
+      damege = self.offense - brave.defense
+    end
+
+    brave.hp -= damege
+
+    p <<~TEXT
+      #{self.name}は#{brave.name}に#{damege}のダメージを与えた
+      #{brave.name}の残りHPは#{brave.hp}だ
+    TEXT
+  end
+
+  def calculate_special_attack
+    self.offense *=  POWER_UP_RATE
+  end
+
+  private
+
+  def transformation
+    self.transformed = true
+    p "#{self.name}は怒った"
+    p "#{self.name}はキングスライムに変身した"
+    self.name = 'キングスライム'
+    calculate_special_attack
+  end
+
 end
 
 brave = Brave.new(name: '勇者', hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: 'スライム', hp: 250, offense: 200, defense: 100)
 
 brave.attack(monster)
+brave.attack(monster)
+
+monster.attack(brave)
+monster.attack(brave)
+
 
 
 
